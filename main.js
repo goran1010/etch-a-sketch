e31a720dcfae43eba55a17a4344a7a19130fef1b
Goran Jovic <goran1010jovic@gmail.com>
2022-07-02 21:41:40 +0200

    Got it to work,but needs cleanup

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-const containerDiv = document.querySelector(`#container`);
+let containerDiv = document.querySelector(`.container`);
+const body = document.querySelector(`body`);
 
-let columnNumber = 16;
-let rowNumber = 16;
-
-const button = document.querySelector(`button`);
-button.addEventListener(`click`, () => {
-  columnNumber = prompt(`Choose number of columns: `, 16);
-  rowNumber = prompt(`Choose number of rows: `, 16);
-  startGame();
-});
+let columnNumber = 48;
+let rowNumber = 48;
+let columnReset;
+let rowReset;
 
 startGame();
+
+const button = document.querySelector(`button`);
+
+button.addEventListener(`click`, () => {
+  body.removeChild(containerDiv);
+  columnNumber = prompt(`Type in the columns: `, 48);
+  rowNumber = prompt(`Type in the rows: `, 48);
+
+  containerDiv = document.createElement(`div`);
+  containerDiv.classList.add(`container`);
+  body.appendChild(containerDiv);
+
+  startGame();
+});
 
 function startGame() {
   for (let i = 0; i < columnNumber; i++) {
     column = document.createElement(`div`);
     containerDiv.appendChild(column);
     column.setAttribute(`class`, `column`);
+    column.setAttribute(`id`, `column[${i}]`);
     for (let j = 0; j < rowNumber; j++) {
       row = document.createElement(`div`);
       row.setAttribute(`class`, `row`);
       row.setAttribute(`id`, `column[${i}]row[${j}]`);
       column.appendChild(row);
     }
+  }
+  const rowsBox = Array.from(document.querySelectorAll(`.row`));
+  for (let i = 0; i < rowNumber * columnNumber; i++) {
+    rowsBox[i].addEventListener(`mouseenter`, () =>
+      rowsBox[i].classList.add("active")
+    );
   }
 }
